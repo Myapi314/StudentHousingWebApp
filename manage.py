be164aee20d8e4e9bc89277eb8444ad4dd32975a
Myapi314 <97209406+Myapi314@users.noreply.github.com>
2023-06-12 11:50:23 -0600

    django tutorial

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'housing_web_app.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'housingapp.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
